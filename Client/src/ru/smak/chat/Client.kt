8c34123cd9d5aa0b28326ac212f15479a4c79307
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2025-04-07 10:06:27 +0300

    Chat v.1 Чат клиента с самим собой

--- Client/src/ru/smak/chat/Client.kt
+++ Client/src/ru/smak/chat/Client.kt
@@ -3,24 +3,51 @@ package ru.smak.chat
 import java.io.PrintWriter
 import java.net.Socket
 import java.util.Scanner
+import kotlin.concurrent.thread
 
 class Client(
     val host: String,
     val port: Int,
 ) {
     private val socket: Socket = Socket(host, port)
+    private var isRunning = true
+    private val scanner = Scanner(socket.getInputStream())
+    private val writer = PrintWriter(socket.getOutputStream())
+
+    private val userScanner = Scanner(System.`in`)
 
     init{
-        val writer = PrintWriter(socket.getOutputStream())
+        startMessageAccepting()
 
-        val userScanner = Scanner(System.`in`)
-        val userInput = userScanner.nextLine()
+        var userInput = "-"
+        thread {
+            while (userInput.isNotBlank()) {
+                userInput = userScanner.nextLine()
+                sendMessage(userInput)
+            }
+        }
+    }
 
-        writer.println(userInput)
+    private fun startMessageAccepting(){
+        thread {
+            while(isRunning){
+                val data = scanner.nextLine()
+                parse(data)
+            }
+            socket.close()
+        }
+    }
+
+    private fun parse(data: String){
+        println("От сервера пришло: $data")
+    }
+
+    fun stop(){
+        isRunning = false
+    }
+
+    fun sendMessage(message: String){
+        writer.println(message)
         writer.flush()
-        val scanner = Scanner(socket.getInputStream())
-        val serverData = scanner.nextLine()
-        println(serverData)
-        socket.close()
     }
 }
